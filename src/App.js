a7dcb79a4053983f9af44c00d791deccf25c4102
AdonaiJehosua <adonaijehosua@gmail.com>
2022-11-11 21:21:31 +0500

    FirstCommit: complite js version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Observer } from 'mobx-react-lite';
+import './App.scss';
+import { TodododoSetter } from './components/TodododoSetter';
+import { ToDododosList } from './components/ToDododosList';
+
+
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Observer>
+      {() => {
+        return (
+          <div className="App">
+            <div className="App_header">ToDoDoList!</div>
+            <TodododoSetter />
+            <ToDododosList />
+          </div>
+        )
+      }}
+    </Observer>
   );
 }
 
